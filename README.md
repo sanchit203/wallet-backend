689fa148f1fa07d77aaca04f8aae0cdd28d9c508
Sanchit <sanchitmunjal203@gmail.com>
2024-03-30 14:15:32 +0530

    added database connection

---
+++ README.md
@@ -0,0 +1,1 @@
+# Spring Boot Wallet API README.md
---
+++ src/test/java/com/walletbackend/WalletBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walletbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
